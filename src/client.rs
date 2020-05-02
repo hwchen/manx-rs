58289f45c27caf9b1b51b13566a020c0cab4e65d
Walther Chen <walther.chen@gmail.com>
2020-05-02 18:06:00 -0400

    upgrade to async backed by smol

--- src/client.rs
+++ src/client.rs
@@ -1,50 +1,100 @@
 use ansi_term::Colour::{Green, Red, White};
 use anyhow::Result;
+use async_tungstenite::tungstenite::Message;
+use futures::stream::StreamExt;
 use rl_sys::readline::{self, redisplay};
 use rl_sys::history::{listmgmt, mgmt};
+use smol::{Async, Task};
 use std::io::{self, Write};
+use std::net::TcpStream;
 use std::process;
 use std::sync::mpsc::channel;
 use std::thread;
-use tungstenite::Message;
 use url::Url;
 
+// Three threads:
+// - stdin loop
+// - stdout loop
+// - websocket async (read and write tasks spawned)
+//
+// Use channels to communicate across threads.
+// - Crossbeam channel when receiver is in sync stdout
+// - piper when receiver is in websocket async
+//
+// First just support ws, not wss
 pub fn wscat_client(url: Url, _auth_option: Option<String>) -> Result<()> {
-    let (ws, _response) = tungstenite::connect(url)?;
-    use std::sync::{Arc, Mutex};
-    let ws = Arc::new(Mutex::new(ws));
+    // set up channels for communicating
+    let (tx_to_stdout, rx_stdout) = channel::<Message>(); // async -> sync
+    let (tx_to_ws_write, rx_ws_write) = piper::chan::<Message>(10); // sync -> async, async -> async
 
+    let chans = WsChannels {
+        tx_to_ws_write: tx_to_ws_write.clone(),
+        tx_to_stdout,
+        rx_ws_write,
+    };
 
-    // channel for sending messages from readline to ws send thread
-    let (tx, rx) = channel();
+    // run read/write tasks for websocket
+    thread::spawn(|| smol::run(ws_client(url, chans)));
 
-    let ws_1 = ws.clone();
-    // Thread for sending to ws
-    let send = thread::spawn(move || {
-        loop {
-            let message: Message = rx.recv().unwrap();
-            println!("message received for writing to websocket");
-
-            let mut ws = ws_1.lock().unwrap();
-            println!("websocket unlocked for read");
-            if let Err(err) = ws.write_message(message) {
-                let out = format!("Connection Closed: {}", err);
-                println!("");
-                println!("{}", Red.paint(out));
-                process::exit(1);
+    //stdout loop
+    thread::spawn(|| {
+        for message in rx_stdout {
+            if !(message.is_text() || message.is_binary()) {
+                continue;
             }
+
+            redisplay::save_prompt();
+
+            //clear line, maybe there's easier way in readline
+            // the 2K is to clear line completely
+            // the 2D is to move cursor back two spaces (from where it is
+            // after clearing the line, goes to original cursor position)
+            // Hmm... something weird happened. Now I'm using 1G to move to
+            // beginning of line. Not sure what changed from last version.
+            let esc = String::from_utf8(vec![27]).unwrap();
+            let clear_line_bytes = format!("{}[2K{}[1G", esc, esc).into_bytes();
+            io::stdout().write(&clear_line_bytes).expect("error clearing line");
+
+            io::stdout().write(&message.into_text().unwrap().as_bytes()).unwrap();
+            io::stdout().flush().unwrap();
+            redisplay::on_new_line().unwrap();
+            redisplay::rl_restore_prompt();
+            redisplay::redisplay();
         }
     });
 
-    // Thread for receiving from ws
-    let tx_1 = tx.clone();
-    let ws_2 = ws.clone();
-    let receive = thread::spawn(move || {
-        loop {
-            // ugh, this is ridiculous to have a mutex here. But I'm going to split when moving to
-            // async anyways.
-            let mut ws = ws_2.lock().unwrap();
-            let message: Message = match ws.read_message() {
+    // stdin loop
+    loop {
+        let input = match readline::readline("> ") {
+            Ok(Some(i)) => i,
+            Ok(None) => continue,
+            _ => break,
+        };
+        listmgmt::add(&input).unwrap();
+        // block on this
+        let _ = smol::block_on(async {tx_to_ws_write.send(Message::text(input)).await});
+
+        println!("message sent for writing to websocket");
+    }
+    mgmt::cleanup();
+
+    Ok(())
+}
+
+// only use thread-local executor, since smol will only run on one thread
+async fn ws_client(addr: Url, chans: WsChannels) -> Result<()> {
+    let WsChannels {tx_to_ws_write, tx_to_stdout, rx_ws_write } = chans;
+    let tx_to_ws_write = tx_to_ws_write.clone();
+
+    let stream = Async::<TcpStream>::connect(&addr).await?;
+    let (stream, _resp) = async_tungstenite::client_async(&addr, stream).await?;
+
+    let (writer, mut reader) = stream.split();
+
+    // read task reads from ws, then sends signal to stdout loop
+    let read_task = Task::local(async move {
+        while let Some(message) = reader.next().await {
+            let message: Message = match message {
                 Ok(m) => m,
                 Err(err) => {
                     let out = format!("Connection Closed: {}", err);
@@ -57,7 +107,7 @@ pub fn wscat_client(url: Url, _auth_option: Option<String>) -> Result<()> {
             //write to stdout depending on opcode
             let out = match message {
                 Message::Ping(payload) => {
-                    tx_1.send(Message::Pong(payload)).unwrap();
+                    tx_to_ws_write.send(Message::Pong(payload)).await;
                     format!("{}", Green.paint("Ping!\n")) //add color
                 },
                 Message::Text(payload) => {
@@ -78,46 +128,25 @@ pub fn wscat_client(url: Url, _auth_option: Option<String>) -> Result<()> {
                 _ => format!("Unsupported ws message"),
             };
 
-            redisplay::save_prompt();
-
-            //clear line, maybe there's easier way in readline
-            // the 2K is to clear line completely
-            // the 2D is to move cursor back two spaces (from where it is
-            // after clearing the line, goes to original cursor position)
-            // Hmm... something weird happened. Now I'm using 1G to move to
-            // beginning of line. Not sure what changed from last version.
-            let esc = String::from_utf8(vec![27]).unwrap();
-            let clear_line_bytes = format!("{}[2K{}[1G", esc, esc).into_bytes();
-            io::stdout().write(&clear_line_bytes).expect("error clearing line");
-
-            io::stdout().write(&out.as_bytes()).unwrap();
-            io::stdout().flush().unwrap();
-            redisplay::on_new_line().unwrap();
-            redisplay::rl_restore_prompt();
-            redisplay::redisplay();
+            // blocking
+            // TODO try crossbeam channel?
+            tx_to_stdout.send(Message::text(out)).unwrap();
         }
     });
+    read_task.detach();
 
-    loop {
-        let input = match readline::readline("> ") {
-            Ok(Some(i)) => i,
-            Ok(None) => continue,
-            _ => break,
-        };
-        listmgmt::add(&input).unwrap();
-        let _ = tx.send(Message::text(input));
-        println!("message sent for writing to websocket");
-    }
-
-    mgmt::cleanup();
-
-    // unwrap which exits program
-    send.join().unwrap();
-    receive.join().unwrap();
+    // TODOD remove this unwrap
+    let write_task = Task::local(rx_ws_write.map(Ok).forward(writer));
+    write_task.unwrap().detach();
 
     Ok(())
 }
 
+struct WsChannels {
+    tx_to_ws_write: piper::Sender<Message>,
+    tx_to_stdout: std::sync::mpsc::Sender<Message>,
+    rx_ws_write: piper::Receiver<Message>,
+}
 
 
 // TODO do this later
