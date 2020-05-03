95b36e2bbc54f363c5bb5cfcf31e9aa9dae6af17
Walther Chen <walther.chen@gmail.com>
2020-05-03 11:00:40 -0400

    remove server subcommand, no subcommands

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,21 @@ fn main() -> Result<()> {
     // Command line interface
     let matches = cli::get_cli();
 
-    // Startup client or server
-    if let Some(ref matches) = matches.subcommand_matches("connect") {
-        if let Some(url_option) = matches.value_of("URL") {
-            let url: Url = url_option.parse()
-                .with_context(|| format!("Error parsing {:?}", url_option))?;
+    if let Some(url_option) = matches.value_of("URL") {
+        let url: Url = url_option.parse()
+            .with_context(|| format!("Error parsing {:?}", url_option))?;
 
-            // TODO later
-            //let auth_option = matches.value_of("USERNAME:PASSWORD")
-            //    .and_then(|user_pass| {
-            //        parse_authorization(user_pass)
-            //    });
-            let auth_option = None;
+        // TODO later
+        //let auth_option = matches.value_of("USERNAME:PASSWORD")
+        //    .and_then(|user_pass| {
+        //        parse_authorization(user_pass)
+        //    });
+        let auth_option = None;
 
-            // print that client is connecting
-            let out_url = format!("Connected to {:?} (Ctrl-C to exit)", url_option);
-            println!("{}", Blue.bold().paint(out_url));
-            client::wscat_client(url, auth_option)?;
-        }
+        // print that client is connecting
+        let out_url = format!("Connected to {:?} (Ctrl-C to exit)", url_option);
+        println!("{}", Blue.bold().paint(out_url));
+        client::wscat_client(url, auth_option)?;
     }
 
     Ok(())
